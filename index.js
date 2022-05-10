49760326a6769cd55ca5c8f43a1533972d8e62ef
Lev <lguzm038@uottawa.ca>
2022-05-10 08:00:15 -0500

    Add template engine

--- index.js
+++ index.js
@@ -5,12 +5,51 @@
 
 const Koa = require("koa");
 const Router = require("@koa/router");
+const render = require("koa-ejs"); //template engine
+const path = require("path");
 
 const app = new Koa();
 const router = new Router();
 
+render(app, {
+  root: path.join(__dirname, "views"),
+  layout: "index",
+  viewExt: "html",
+  cache: false,
+  debug: true,
+});
+
 router.get("hello-koa", "/", (ctx) => {
-  ctx.body = "Hello Koa";
+  let koalaFacts = [];
+
+  koalaFacts.push({
+    meta_name: "Color",
+    meta_value: "Black and white",
+  });
+
+  koalaFacts.push({
+    meta_name: "Native Country",
+    meta_value: "Australia",
+  });
+
+  koalaFacts.push({
+    meta_name: "Animal Classification",
+    meta_value: "Mammal",
+  });
+
+  koalaFacts.push({
+    meta_name: "Life Span",
+    meta_value: "13 - 18 years",
+  });
+
+  koalaFacts.push({
+    meta_name: "Are they bears?",
+    meta_value: "No",
+  });
+
+  return ctx.render("index", {
+    attributes: koalaFacts,
+  });
 });
 
 app.use(router.routes()).use(router.allowedMethods());
