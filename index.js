ebf9659550f3fbd647977be41330840a9d8550a0
Lev <lguzm038@uottawa.ca>
2022-05-10 07:55:12 -0500

    Add koa router

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 "use strict";
-const Koa = require("koa");
-const app = new Koa();
 
-app.use((ctx) => {
-  ctx.body = "Hello World , this is my first koa app";
+//Koa does not come with any middleware
+//Need to import additional packages for richer functionality
+
+const Koa = require("koa");
+const Router = require("@koa/router");
+
+const app = new Koa();
+const router = new Router();
+
+router.get("hello-koa", "/", (ctx) => {
+  ctx.body = "Hello Koa";
 });
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000);
